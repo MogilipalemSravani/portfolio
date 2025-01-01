7eea5643d6d9a0b278550b6a55a962c7a2dcbfc5
MogilipalemSravani <157085813+MogilipalemSravani@users.noreply.github.com>
2025-01-01 17:29:26 +0530

    Update main.js

--- main.js
+++ main.js
@@ -10,14 +10,14 @@ menuIcon.onclick = () =>{
 let sections=document.querySelectorAll('section');
 let navLinks=document.querySelectorAll('header nav a');
 window.onscroll = () =>{
-    sections.forEach(sec=>{
+    sections.forEach((sec)=>{
         let top=window.scrollY;
         let offset=sec.offsetTop - 150;
         let height=sec.offsetHeight;
         let id=sec.getAttribute('id');
 
         if(top >=offset && top < offset + height){
-            navLinks.forEach.apply(links=>{
+            navLinks.forEach.apply((links)=>{
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
