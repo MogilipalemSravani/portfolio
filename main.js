1f4753239c16e335c1f2288c64d63489eadb1f00
MogilipalemSravani <157085813+MogilipalemSravani@users.noreply.github.com>
2025-01-01 17:26:25 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,10 @@
 let menuIcon=document.querySelector('#menu-icon');
 let navbar=document.querySelector('.navbar');
 
-menuIcon.onClick = () =>{
+menuIcon.onclick = () =>{
     menuIcon.classList.toggle( 'fa-xmark');
     navbar.classList.toggle('active');
-}
+};
 /*===================scroll section active link===========*/
 let sections=document.querySelectorAll('section');
 let navLinks=document.querySelectorAll('header nav a');
